cd16b0ddbe58b11e9dfaa779900e468191af7b6e
pietrolupi <pietro.lupi92@gmail.com>
2023-11-08 15:43:46 +0100

    [add]: altro controllo errore funzionante

--- Model/Production.php
+++ Model/Production.php
@@ -18,12 +18,20 @@ class Production{
       $this->genres = $_genres;
     }
 
+    if(($_viewer_rating > 10 || $_viewer_rating < 0) || ($_critique_rating > 10 || $_critique_rating < 0)  ){
+      throw new Exception("Insert values beetween 0 and 10 for viewer rating and critique rating.");
+    }else{
+
+      $this->viewer_rating = $_viewer_rating;
+      $this->critique_rating = $_critique_rating;
+
+    }
+
+
     $this->title = $_title;
     $this->description = $_description;
     $this->poster = $_poster;
     //derivate dal trait
-    $this->viewer_rating = $_viewer_rating;
-    $this->critique_rating = $_critique_rating;
   }
 
 }
